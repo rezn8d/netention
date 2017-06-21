b81d9e051ed1d675fc97c29d9114b230d8fa0f45
SeH <seh1seh@yandex.com>
2017-06-21 14:45:50 -0400

    p2, pixi graph view

--- spacegraph.js
+++ spacegraph.js
@@ -4,20 +4,60 @@ class SpaceGraphView extends NView {
 
     constructor() {
         super();
-        console.log('start');
     }
 
     build(me, target) {
-        console.log('init graph view');
         var ele = undefined;
         $LAB
             .script('lib/p2.min.js')
             .script('lib/pixi.min.js')
-            .script('lib/p2.renderer.min.js')
+            .script('lib/p2.renderer.custom.js')
+            .script('lib/dat.gui.min.js')
             .wait()
             .wait(()=>{
-                console.log('starting graph view');
-                ele = $('<h1>GRAPH</h1>').appendTo(target);
+                console.log('starting graph view', p2);
+
+                const world = new p2.World({
+                    gravity: [0, 0]
+                });
+
+
+                const sg = new p2.WebGLRenderer(function () {
+
+                    this.setWorld(world);
+
+
+                    world.on("addBody", function (evt) {
+                        evt.body.setDensity(1);
+                    });
+
+                    // Change the current engine torque with the left/right keys
+                    this.on("keydown", function (evt) {
+                        // t = 5;
+                        // switch (evt.keyCode) {
+                        //     case 39: // right
+                        //         torque = -t;
+                        //         break;
+                        //     case 37: // left
+                        //         torque = t;
+                        //         break;
+                        // }
+                    });
+                    this.on("keyup", function () {
+                        //torque = 0;
+                    });
+                });
+                target.html(sg.element);
+
+                sg.p2 = p2;
+
+
+                for (var i = 0; i < 25; i++) {
+                    var body = new p2.Body({mass: 1, position: [Math.random() * 3, Math.random() * 3]});
+                    var shape = new p2.Box({width: 0.5, height: 0.5});
+                    body.addShape(shape);
+                    sg.world.addBody(body);
+                }
             });
 
         return {
