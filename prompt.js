82ad944e8db142863fde637405c72bd2e8375d13
SeH <seh1seh@yandex.com>
2017-06-23 19:59:02 -0400

    prompt updates

--- prompt.js
+++ prompt.js
@@ -3,19 +3,27 @@
 /** manages the state of an input text prompt and its related pop-ups */
 class Prompt {
 
-    constructor(I, ele) {
+    constructor(me, ele) {
         this.ele = ele;
 
         const that = this;
-        LazyLoad.js("lib/codemirror/codemirror.min.js", ()=>{
 
-            LazyLoad.css('lib/codemirror/codemirror.css');
+        let loading = $('<h1>LOADING EDITOR</h1>');
+        ele.append(loading);
 
+        //LazyLoad.js('lib/wysiwyg-editor.min.js', ()=>{
+         LazyLoad.js("lib/codemirror/codemirror.min.js", () => {
+        //
+        //    LazyLoad.css('lib/wysiwyg-editor.min.css');
+             LazyLoad.css('lib/codemirror/codemirror.css');
+
+            loading.remove();
 
             const editor = that.editor = CodeMirror(ele[0], {
                 lineNumbers: false,
                 viewportMargin: Infinity
             });
+
 
 
             //const suggFeed = D();
@@ -28,42 +36,88 @@ class Prompt {
             //editor.addLineWidget(1, suggFeed[0], {coverGutter: false, noHScroll: true})
 
 
+            var multiline = false;
+
+            var f;
+
+            function reset() {
+
+                if (f)
+                    f.remove();
+
+                f = D();
+
+                multiline = false;
+                // editor.setValue('');
+                // editor.addLineWidget(2, f[0], {
+                //
+                // });
+            }
+            reset();
+
+            function close() {
+                $(ele).fadeOut();
+                reset();
+            }
+
             editor.addKeyMap({
 
                 //TODO ctrl-enter
 
-                'Esc': function(cm) {
-                    cm.setValue('');
-                    editor.fadeOut();
+                'Enter': cm => {
+                    if (!multiline) {
+                        const val = cm.getValue().trim();
+                        cm.setValue('');
+                        {
+
+                            //add finder
+                        }
+                    }
+                    return CodeMirror.Pass;
+                },
+                'Down': cm => {
+                    if (!multiline) {
+                        $(ele[0]).addClass('multiline');
+                        multiline = true;
+                        f.remove();
+                    }
+                    return CodeMirror.Pass;
+                },
+
+                'Esc': cm => {
+
+                    close();
                 }
             });
-
 
 
             var q;
             let queryMinUpdatePeriodMS = 100;
             const onQueryTextChanged = _.throttle(() => {
 
-                const nextQ = editor.getValue();
+                if (multiline)
+                    return;
+
+                const nextQ = editor.getValue().trim();
                 if (nextQ == q)
                     return; //same
 
                 q = nextQ;
+                if (!q.length) {
+                    reset();
+                }
+
                 //editor.setValue(editor.getLine(0)); //clear everything below first line
 
                 //suggFeed.html('');
 
 
-
-
-
                 // const update = _.debounce(()=>suggFeed.packery( 'appended' ), 100);
                 //
-                I.get(q, (x)=>{
+                me.get(q, (x) => {
 
                     const y = new NIcon(x).ele;
-
-                    editor.addWidget({ch:30 , line: 2}, y[0]);
+                    f.append(y);
 
 
                     //editor.append(y);
@@ -93,7 +147,7 @@ class Prompt {
                     //update(q);
                     //I.info("share: " + q);
 
-                    I.put({
+                    me.put({
                         I: 'x' + parseInt(10000000 * Math.random()), //TODO UUID
                         N: q
                     });
@@ -104,11 +158,10 @@ class Prompt {
 
 
             //clicking on the icon causes editor to focus
-            ele.click(()=>{
+            ele.click(() => {
                 editor.focus();
             });
             editor.focus(); //auto-focus
-
 
 
         });
